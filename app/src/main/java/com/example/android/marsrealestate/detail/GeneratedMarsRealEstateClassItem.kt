aae9c96a5c6dbf72cfa27ccb83ce079eefcb7f99
Archangel <rduncan354@gmail.com>
2022-12-20 20:04:21 -0500

    testing to see if I can committ

---
+++ app/src/main/java/com/example/android/marsrealestate/detail/GeneratedMarsRealEstateClassItem.kt
@@ -0,0 +1,8 @@
+package com.example.android.marsrealestate.detail
+
+data class GeneratedMarsRealEstateClassItem(
+    val id: String,
+    val img_src: String,
+    val price: Int,
+    val type: String
+)
